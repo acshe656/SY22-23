66ce4af9a6e5dcc7c44e432b9e0fd2a078443b8b
Ashley <acshe656@learner.dvusd.org>
2022-08-18 07:38:38 -0700

    Wednesday Update

---
+++ VB/Planet Dex/Planet Dex/Form1.Designer.vb
@@ -0,0 +1,358 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.VenusButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.VenusName = New System.Windows.Forms.Label()
+        Me.VenusColor = New System.Windows.Forms.Label()
+        Me.VenusSize = New System.Windows.Forms.Label()
+        Me.venustemp = New System.Windows.Forms.Label()
+        Me.VenusDistance = New System.Windows.Forms.Label()
+        Me.NeptuneButton = New System.Windows.Forms.Button()
+        Me.MercuryButton = New System.Windows.Forms.Button()
+        Me.SaturnButton = New System.Windows.Forms.Button()
+        Me.PlutoButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.saturnPictureBox = New System.Windows.Forms.PictureBox()
+        Me.MercuryPictureBox = New System.Windows.Forms.PictureBox()
+        Me.neptunePictureBox = New System.Windows.Forms.PictureBox()
+        Me.VenusPicture = New System.Windows.Forms.PictureBox()
+        Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.plutomesage = New System.Windows.Forms.Label()
+        Me.Title = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.saturnPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MercuryPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.neptunePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VenusPicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'VenusButton
+        '
+        Me.VenusButton.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VenusButton.ForeColor = System.Drawing.Color.BlueViolet
+        Me.VenusButton.Location = New System.Drawing.Point(13, 342)
+        Me.VenusButton.Name = "VenusButton"
+        Me.VenusButton.Size = New System.Drawing.Size(119, 48)
+        Me.VenusButton.TabIndex = 0
+        Me.VenusButton.Text = "Venus"
+        Me.VenusButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label1.Location = New System.Drawing.Point(347, 200)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label2.Location = New System.Drawing.Point(347, 221)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Color:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label3.Location = New System.Drawing.Point(347, 242)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Size:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label4.Location = New System.Drawing.Point(347, 264)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Temprature:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label5.Location = New System.Drawing.Point(347, 289)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(183, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Distance from the sun:"
+        '
+        'VenusName
+        '
+        Me.VenusName.AutoSize = True
+        Me.VenusName.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VenusName.ForeColor = System.Drawing.Color.BlueViolet
+        Me.VenusName.Location = New System.Drawing.Point(527, 203)
+        Me.VenusName.Name = "VenusName"
+        Me.VenusName.Size = New System.Drawing.Size(0, 16)
+        Me.VenusName.TabIndex = 8
+        '
+        'VenusColor
+        '
+        Me.VenusColor.AutoSize = True
+        Me.VenusColor.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VenusColor.ForeColor = System.Drawing.Color.BlueViolet
+        Me.VenusColor.Location = New System.Drawing.Point(530, 225)
+        Me.VenusColor.Name = "VenusColor"
+        Me.VenusColor.Size = New System.Drawing.Size(0, 16)
+        Me.VenusColor.TabIndex = 9
+        '
+        'VenusSize
+        '
+        Me.VenusSize.AutoSize = True
+        Me.VenusSize.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VenusSize.ForeColor = System.Drawing.Color.BlueViolet
+        Me.VenusSize.Location = New System.Drawing.Point(530, 245)
+        Me.VenusSize.Name = "VenusSize"
+        Me.VenusSize.Size = New System.Drawing.Size(0, 16)
+        Me.VenusSize.TabIndex = 10
+        '
+        'venustemp
+        '
+        Me.venustemp.AutoSize = True
+        Me.venustemp.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.venustemp.ForeColor = System.Drawing.Color.BlueViolet
+        Me.venustemp.Location = New System.Drawing.Point(533, 266)
+        Me.venustemp.Name = "venustemp"
+        Me.venustemp.Size = New System.Drawing.Size(0, 16)
+        Me.venustemp.TabIndex = 11
+        '
+        'VenusDistance
+        '
+        Me.VenusDistance.AutoSize = True
+        Me.VenusDistance.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VenusDistance.ForeColor = System.Drawing.Color.BlueViolet
+        Me.VenusDistance.Location = New System.Drawing.Point(530, 289)
+        Me.VenusDistance.Name = "VenusDistance"
+        Me.VenusDistance.Size = New System.Drawing.Size(0, 16)
+        Me.VenusDistance.TabIndex = 12
+        '
+        'NeptuneButton
+        '
+        Me.NeptuneButton.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NeptuneButton.ForeColor = System.Drawing.Color.BlueViolet
+        Me.NeptuneButton.Location = New System.Drawing.Point(173, 342)
+        Me.NeptuneButton.Name = "NeptuneButton"
+        Me.NeptuneButton.Size = New System.Drawing.Size(116, 49)
+        Me.NeptuneButton.TabIndex = 13
+        Me.NeptuneButton.Text = "Neptune"
+        Me.NeptuneButton.UseVisualStyleBackColor = True
+        '
+        'MercuryButton
+        '
+        Me.MercuryButton.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MercuryButton.ForeColor = System.Drawing.Color.BlueViolet
+        Me.MercuryButton.Location = New System.Drawing.Point(344, 340)
+        Me.MercuryButton.Name = "MercuryButton"
+        Me.MercuryButton.Size = New System.Drawing.Size(116, 49)
+        Me.MercuryButton.TabIndex = 15
+        Me.MercuryButton.Text = "Mercury"
+        Me.MercuryButton.UseVisualStyleBackColor = True
+        '
+        'SaturnButton
+        '
+        Me.SaturnButton.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SaturnButton.ForeColor = System.Drawing.Color.BlueViolet
+        Me.SaturnButton.Location = New System.Drawing.Point(513, 340)
+        Me.SaturnButton.Name = "SaturnButton"
+        Me.SaturnButton.Size = New System.Drawing.Size(116, 50)
+        Me.SaturnButton.TabIndex = 17
+        Me.SaturnButton.Text = "Saturn"
+        Me.SaturnButton.UseVisualStyleBackColor = True
+        '
+        'PlutoButton
+        '
+        Me.PlutoButton.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PlutoButton.ForeColor = System.Drawing.Color.BlueViolet
+        Me.PlutoButton.Location = New System.Drawing.Point(672, 340)
+        Me.PlutoButton.Name = "PlutoButton"
+        Me.PlutoButton.Size = New System.Drawing.Size(116, 51)
+        Me.PlutoButton.TabIndex = 19
+        Me.PlutoButton.Text = "Pluto"
+        Me.PlutoButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Planet_Dex.My.Resources.Resources.Pluto_whole_color
+        Me.PictureBox1.Location = New System.Drawing.Point(173, 397)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(116, 67)
+        Me.PictureBox1.TabIndex = 20
+        Me.PictureBox1.TabStop = False
+        '
+        'saturnPictureBox
+        '
+        Me.saturnPictureBox.Image = Global.Planet_Dex.My.Resources.Resources._1920x1080
+        Me.saturnPictureBox.Location = New System.Drawing.Point(672, 398)
+        Me.saturnPictureBox.Name = "saturnPictureBox"
+        Me.saturnPictureBox.Size = New System.Drawing.Size(116, 67)
+        Me.saturnPictureBox.TabIndex = 18
+        Me.saturnPictureBox.TabStop = False
+        '
+        'MercuryPictureBox
+        '
+        Me.MercuryPictureBox.Image = Global.Planet_Dex.My.Resources.Resources.PFQ97KNjjTebMzenT3GeKd_1200_80
+        Me.MercuryPictureBox.Location = New System.Drawing.Point(513, 398)
+        Me.MercuryPictureBox.Name = "MercuryPictureBox"
+        Me.MercuryPictureBox.Size = New System.Drawing.Size(116, 67)
+        Me.MercuryPictureBox.TabIndex = 16
+        Me.MercuryPictureBox.TabStop = False
+        '
+        'neptunePictureBox
+        '
+        Me.neptunePictureBox.Image = Global.Planet_Dex.My.Resources.Resources.maxresdefault
+        Me.neptunePictureBox.Location = New System.Drawing.Point(344, 398)
+        Me.neptunePictureBox.Name = "neptunePictureBox"
+        Me.neptunePictureBox.Size = New System.Drawing.Size(116, 67)
+        Me.neptunePictureBox.TabIndex = 14
+        Me.neptunePictureBox.TabStop = False
+        '
+        'VenusPicture
+        '
+        Me.VenusPicture.Image = Global.Planet_Dex.My.Resources.Resources.Venus___December_23_2016
+        Me.VenusPicture.Location = New System.Drawing.Point(12, 397)
+        Me.VenusPicture.Name = "VenusPicture"
+        Me.VenusPicture.Size = New System.Drawing.Size(116, 67)
+        Me.VenusPicture.TabIndex = 2
+        Me.VenusPicture.TabStop = False
+        '
+        'DisplayPictureBox
+        '
+        Me.DisplayPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(12, 94)
+        Me.DisplayPictureBox.Name = "DisplayPictureBox"
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(318, 230)
+        Me.DisplayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.DisplayPictureBox.TabIndex = 1
+        Me.DisplayPictureBox.TabStop = False
+        '
+        'plutomesage
+        '
+        Me.plutomesage.AutoSize = True
+        Me.plutomesage.Location = New System.Drawing.Point(473, 149)
+        Me.plutomesage.Name = "plutomesage"
+        Me.plutomesage.Size = New System.Drawing.Size(0, 17)
+        Me.plutomesage.TabIndex = 21
+        '
+        'Title
+        '
+        Me.Title.AutoSize = True
+        Me.Title.Font = New System.Drawing.Font("Courier New", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Title.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Title.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Title.Location = New System.Drawing.Point(63, 26)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(697, 37)
+        Me.Title.TabIndex = 22
+        Me.Title.Text = "Ashley's Favorite Planets in Order"
+        Me.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Planet_Dex.My.Resources.Resources._0_63257000_1651672813_galaxy
+        Me.ClientSize = New System.Drawing.Size(800, 478)
+        Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.plutomesage)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PlutoButton)
+        Me.Controls.Add(Me.saturnPictureBox)
+        Me.Controls.Add(Me.SaturnButton)
+        Me.Controls.Add(Me.MercuryPictureBox)
+        Me.Controls.Add(Me.MercuryButton)
+        Me.Controls.Add(Me.neptunePictureBox)
+        Me.Controls.Add(Me.NeptuneButton)
+        Me.Controls.Add(Me.VenusDistance)
+        Me.Controls.Add(Me.venustemp)
+        Me.Controls.Add(Me.VenusSize)
+        Me.Controls.Add(Me.VenusColor)
+        Me.Controls.Add(Me.VenusName)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.VenusPicture)
+        Me.Controls.Add(Me.DisplayPictureBox)
+        Me.Controls.Add(Me.VenusButton)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.saturnPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MercuryPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.neptunePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VenusPicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VenusButton As Button
+    Friend WithEvents DisplayPictureBox As PictureBox
+    Friend WithEvents VenusPicture As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents VenusName As Label
+    Friend WithEvents VenusColor As Label
+    Friend WithEvents VenusSize As Label
+    Friend WithEvents venustemp As Label
+    Friend WithEvents VenusDistance As Label
+    Friend WithEvents neptunePictureBox As PictureBox
+    Friend WithEvents NeptuneButton As Button
+    Friend WithEvents MercuryPictureBox As PictureBox
+    Friend WithEvents MercuryButton As Button
+    Friend WithEvents saturnPictureBox As PictureBox
+    Friend WithEvents SaturnButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PlutoButton As Button
+    Friend WithEvents plutomesage As Label
+    Friend WithEvents Title As Label
+End Class
---
+++ VB/Planet Dex/Planet Dex/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub VenusButton_Click(sender As Object, e As EventArgs) Handles VenusButton.Click
+        DisplayPictureBox.Image = VenusPicture.Image
+        VenusName.Text = "Venus"
+        VenusColor.Text = "White/Orange"
+        VenusSize.Text = "3,760.4 mi"
+        VenusDistance.Text = "66.849 million mi"
+        venustemp.Text = "800-900f"
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub NeptuneButton_Click(sender As Object, e As EventArgs) Handles NeptuneButton.Click
+        DisplayPictureBox.Image = neptunePictureBox.Image
+        VenusName.Text = "Neptune"
+        VenusColor.Text = "Blue"
+        VenusSize.Text = "15,299 mi"
+        VenusDistance.Text = "2.78 billion mi"
+        venustemp.Text = "-373 degrees F."
+    End Sub
+
+    Private Sub MercuryButton_Click(sender As Object, e As EventArgs) Handles MercuryButton.Click
+        DisplayPictureBox.Image = MercuryPictureBox.Image
+        VenusName.Text = "Mercury"
+        VenusColor.Text = "Rainbow"
+        VenusSize.Text = "1,516 mi"
+        VenusDistance.Text = "42.702 million mi"
+        venustemp.Text = "750-800f"
+    End Sub
+
+    Private Sub SaturnButton_Click(sender As Object, e As EventArgs) Handles SaturnButton.Click
+        DisplayPictureBox.Image = saturnPictureBox.Image
+        VenusName.Text = "Saturn"
+        VenusColor.Text = "Yellowish"
+        VenusSize.Text = "36,184 mi"
+        VenusDistance.Text = "916.05 million mi"
+        venustemp.Text = "about -285 degrees F."
+    End Sub
+
+    Private Sub PlutoButton_Click(sender As Object, e As EventArgs) Handles PlutoButton.Click
+        DisplayPictureBox.Image = PictureBox1.Image
+        VenusName.Text = "Pluto"
+        VenusColor.Text = "Blueish Redish Grayish"
+        VenusSize.Text = "738.38 mi"
+        VenusDistance.Text = "3.7 billion miles"
+        venustemp.Text = "-387°F"
+        plutomesage.Text = "Always the Biggest Planet in our Hearts<3"
+    End Sub
+End Class
